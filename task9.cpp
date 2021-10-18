0a2858ed43a156a0aa618a440dffc69217640cf4
George <george.mikhail@yandex.ru>
2021-10-18 13:23:47 +0300

    added constructor

--- task9.cpp
+++ task9.cpp
@@ -8,10 +8,12 @@
 int main()
 {
 
-	TupleType<2, float, int, string> a;
-	a.a = 0.1;
-	a.b.a = 7;
-	a.b.b.a = "qwe";
+	TupleType<2, float, int, string> a(0.1,5,"asd");
+	a.single = 0.1;
+	a.difficult.single = 7;
+	a.difficult.difficult.single = "qwe";
+	a.operator[]<6>();
+
 	system("pause");
 	return 0;
 }
--- turpleN.h
+++ turpleN.h
@@ -10,15 +10,83 @@ struct TupleType :
 {
     using type1 = T;
     using type2 = TupleType<N - 1, Types...>;
-    type1 a;
-    type2 b;
+    type1 single;
+    type2 difficult;
+
+    TupleType() {};
+
+    TupleType(T arg, Types ... args) :difficult(args...)
+    {
+        single = arg;
+    };
+
+    template<class T, class ... Types>
+    T goToItem(int index)
+    {
+        if (index == N)
+            return single;
+        else
+            difficult.goToItem<Types ...>(index-1);
+    }
+
+
+    template<int k>
+    void some()
+    {
+        return k;
+    }
+
+    
+    
+
+    template<int k>
+    type1 operator [](int )
+    {
+        if (k == N)
+            return single;
+    }
+
+
+    //  template<int index>
+    //  type1 operator [](int )
+    //  {
+    //          return single;
+    //  }
 };
 
-template<typename T>
-struct TupleType<0, T> {
-using type1 = T;
-type1 a;
+template<typename type1>
+struct TupleType<0, type1> {
+    type1 single;
+
+    TupleType() {};
+    TupleType(type1 a)
+    {
+        single = a;
+    }
+
+
+
+    type1 operator [](int )
+    {
+        return single;
+    }
+    
+
 };
 
+template <
+    int N,
+    typename T,
+    typename ... Types
+>
+class Tuple : public TupleType<N, T, Types...>
+{
+public:
 
+
+    auto operator [](int index)
+    {
+        return decltype(TupleType::operator [](index))TupleType::operator [](index);
+    }
+};
 
